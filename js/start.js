0da1a03a12856fa310c59a5d4f2ecc4f1bc4d03c
Tumbl3Weed <calvin.nel00@gmail.com>
2023-11-10 07:40:47 +0000

    Double jump progress

--- js/start.js
+++ js/start.js
@@ -3,7 +3,7 @@
 
 import packageJson from '../package.json'
 
-export default class MyMetaPressPlugin {
+export default class DoubleJumpPlugin {
 
     // Plugin information
     id              = packageJson.metapress?.id || packageJson.name
@@ -11,13 +11,46 @@ export default class MyMetaPressPlugin {
     description     = packageJson.metapress?.description || packageJson.description
     version         = packageJson.version
     provides        = [ ]
-    requires        = [ ]
+    requires = ['entities', 'avatar']
 
+    avatarController = null
+    playerJumped = false;
+    playerDoubleJumped = false;
+    wasGrounded = false;
+    jumpHeldIn = false;
     /** Called on load */
     onLoad() {
+        this.checkAvatarModifier()
+    }
 
-        console.log(`Hello from ${this.name}!`)
+    $onEntityModifierLoad() {
+        this.checkAvatarModifier()
+    }
 
+    checkAvatarModifier() {
+        let modifier = metapress.entities.getModifier(metapress.avatar.currentUserEntity.id, 'avatar-controller')
+        if (this.avatarController == modifier) return
+        this.avatarController = modifier        
+        console.log(this.avatarController)
+    }
+    loggedJump = false
+    $onRender() {
+        if(metapress.input?.actionActive('jump') && !this.loggedJump){
+            console.log(metapress.input?.actionActive('jump'));
+            this.loggedJump = true
+        }
+        if(!metapress.input?.actionActive('jump') && this.loggedJump){
+            console.log(metapress.input?.actionActive('jump'));
+            this.loggedJump = false
+        }
+        if(this.playerJumped && this.playerDoubleJumped == false && metapress.input?.actionActive('jump') && this.jumpHeldIn== false){
+            this.playerDoubleJumped = true;
+            this.avatarController.yVelocity = this.avatarController.entity.avatar_jumpHeight || 5
+            this.avatarController.overrideAnimation({ animationStart: 'jump_start', animation: 'jump_loop', animationEnd: 'jump_end', interruptOnGround: true, pauseAtEnd: true })
+        }
+        if(this.wasGrounded && metapress.input?.actionActive('jump')) {this.playerJumped = true; this.wasGrounded=false;}
+        if (this.avatarController?.isGrounded && this.wasGrounded == false) {this.wasGrounded = true; this.playerJumped = false; this.playerDoubleJumped = false;}
+        this.jumpHeldIn = metapress.input?.actionActive('jump');
     }
 
 }
